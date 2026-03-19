43d9f035c01819f8807398c7518e4a34a18a991b
Ji Joe Asow <jijoeasow24@gmail.com>
2026-03-19 13:31:27 +0400

    Fix Redis env vars for Upstash

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,6 +1,10 @@
 import { Redis } from '@upstash/redis';
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
+
 const KEY = 'lb:data';
 
 export default async function handler(req, res) {
